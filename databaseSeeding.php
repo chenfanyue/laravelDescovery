3d97092dbba46ab51672acc0e04b78c067eac866
chenfanyue <nicegogogo@163.com>
2016-12-13 20:44:46 +0800

    Create databaseSeeding.php

---
+++ databaseSeeding.php
@@ -0,0 +1,54 @@
+// p a make:seeder UsersTableSeeder
+// p a db:seed
+
+<?php
+
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $this->call(UsersTableSeeder::class);
+    }
+}
+
+// -------
+
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory('App\User', 5)->create();
+    }
+}
+
+// -------
+
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+$factory->define(App\User::class, function (Faker\Generator $faker) {
+    static $password;
+
+    return [
+        'name' => $faker->name,
+        'email' => $faker->unique()->safeEmail,
+        'password' => $password ?: $password = bcrypt('secret'),
+        'remember_token' => str_random(10),
+    ];
+});
