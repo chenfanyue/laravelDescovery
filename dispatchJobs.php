fea363a33d4e3b5867d44dfe0b87a77c67e4890d
chenfanyue <nicegogogo@163.com>
2016-12-15 17:06:29 +0800

    Create dispatchJobs.php

---
+++ dispatchJobs.php
@@ -0,0 +1,56 @@
+<?php
+
+Route::get('reports','ReportsController@compile');
+
+
+// another file
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Jobs\CompileReports;
+
+class ReportsController extends Controller
+{
+    public function compile(){
+    	$this->dispatch(new CompileReports);
+    	return 'the job has been dispatched.';
+    }
+}
+
+
+// another file
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class CompileReports implements ShouldQueue
+{
+    use InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        var_dump('compiling in the job instance.');
+    }
+}
