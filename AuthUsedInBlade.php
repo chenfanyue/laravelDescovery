246ea53eee140a66a70aa7d46d98388369cb170b
chenfanyue <nicegogogo@163.com>
2016-12-23 14:02:39 +0800

    Create AuthUsedInBlade.php

---
+++ AuthUsedInBlade.php
@@ -0,0 +1,16 @@
+@if(Auth::check())
+            You are signed in as {{ Auth::user()->username }}
+            @else
+            You are not signed in
+            @endif
+            
+@if (Route::has('login'))
+                <div class="top-right links">
+                    @if (Auth::check())
+                        <a href="{{ url('/home') }}">Home</a>
+                    @else
+                        <a href="{{ url('/login') }}">Login</a>
+                        <a href="{{ url('/register') }}">Register</a>
+                    @endif
+                </div>
+            @endif
